6c52ef57689ce60dbac7c0b72f05dde28005c664
Dung Viet Minh Nguyen <dungvmnguyen@kms.com.vn>
2019-07-28 16:01:45 +0700

    Add BasicInfo section to About Me component.

--- src/components/Forms/AboutMe/AboutMe.jsx
+++ src/components/Forms/AboutMe/AboutMe.jsx
@@ -7,7 +7,7 @@ import ProgressBars from '../../Commons/ProgressBars';
 
 const AboutMe = (props: AboutMeProps) => {
   const {
-    title, greeting, summary, skills,
+    title, greeting, summary, basicInfo, skills,
   } = props;
   return (
     <div
@@ -20,6 +20,23 @@ const AboutMe = (props: AboutMeProps) => {
         <div styleName="introduction">
           <div styleName="greeting">{greeting}</div>
           <div>{summary}</div>
+          <ul styleName="basic-info">
+            {basicInfo.map(info => (
+              <li
+                key={info.name}
+                styleName="info"
+              >
+                <i
+                  styleName="info-icon"
+                  className={info.iconClassName}
+                />
+                <div styleName="info-text">
+                  <span>{info.name}:</span>
+                  <span>&nbsp;{info.value}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
         <div styleName="skills">
           <ProgressBars items={skills} />
--- src/components/Forms/AboutMe/type.js
+++ src/components/Forms/AboutMe/type.js
@@ -1,5 +1,12 @@
+export type SingleInfo = {
+  iconClassName: string,
+  name: string,
+  value: string,
+};
+
 export type AboutMeProps = {
   title: string,
   greeting: string,
   summary: string,
+  basicInfo: Array<SingleInfo>,
 };
