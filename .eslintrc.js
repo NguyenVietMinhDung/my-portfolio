041a44cd2eba8bb4cf361f4509f71db860d769dd
Nguyen Dung <nguyendung@Nguyens-MacBook-Pro.local>
2019-09-13 14:20:07 +0700

    Refactor loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
     'react',
   ],
   rules: {
-    'linebreak-style': ['error', 'windows'],
     'react/jsx-one-expression-per-line': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,6 @@
 // @flow
-import React, { memo, useState, useEffect } from 'react';
-import { HashLoader } from 'react-spinners';
-import CSSModules from 'react-css-modules';
+import React, { memo } from 'react';
 import type { Props } from './type';
-import styles from './App.scss';
 import constants from '../../constants';
 import Forms from '../../components/Forms';
 
@@ -15,69 +12,48 @@ const {
   Navbar, Header, Footer, AboutMe, Experiences, Educations, Projects,
 } = Forms;
 
-const useLoadingStatus = () => {
-  const [loadingStatus, setLoadingStatus] = useState(true);
-  useEffect(() => setLoadingStatus(false));
-  return loadingStatus;
-};
+const App = (props: Props) => (
+  <>
+    <Navbar
+      items={navbar.items}
+      hamburgerIconUrl={navbar.hamburgerIconUrl}
+      closeIconUrl={navbar.closeIconUrl}
+      logoUrl={app.logoUrl}
+      {...props}
+    />
+    <Header
+      name={header.NAME}
+      description={header.DESCRIPTION}
+      btnTxt={header.BUTTON_TEXT}
+      avatarUrl={header.avatarUrl}
+      socialNetworks={app.socialNetworks}
+    />
+    <AboutMe
+      title={aboutMe.TITLE}
+      greeting={aboutMe.GREETING}
+      summary={aboutMe.SUMMARY}
+      basicInfo={aboutMe.basicInfo}
+      skills={aboutMe.skills}
+    />
+    <Experiences
+      title={experiences.TITLE}
+      timeline={experiences.timeline}
+    />
+    <Projects
+      title={projects.TITLE}
+      items={projects.items}
+    />
+    <Educations
+      title={educations.TITLE}
+      timeline={educations.timeline}
+    />
+    <Footer
+      slogan={footer.SLOGAN}
+      copyright={footer.COPYRIGHT}
+      socialNetworks={app.socialNetworks}
+      logoUrl={app.logoUrl}
+    />
+  </>
+);
 
-const App = (props: Props) => {
-  const loadingStatus = useLoadingStatus();
-  return (
-    <>
-      <div styleName="loader">
-        <HashLoader
-          size={app.LOADER_SIZE}
-          sizeUnit={app.LOADER_SIZE_UNIT}
-          color={app.LOADER_COLOR}
-          loading={loadingStatus}
-        />
-      </div>
-      {!loadingStatus && (
-        <>
-          <Navbar
-            items={navbar.items}
-            hamburgerIconUrl={navbar.hamburgerIconUrl}
-            closeIconUrl={navbar.closeIconUrl}
-            logoUrl={app.logoUrl}
-            {...props}
-          />
-          <Header
-            name={header.NAME}
-            description={header.DESCRIPTION}
-            btnTxt={header.BUTTON_TEXT}
-            avatarUrl={header.avatarUrl}
-            socialNetworks={app.socialNetworks}
-          />
-          <AboutMe
-            title={aboutMe.TITLE}
-            greeting={aboutMe.GREETING}
-            summary={aboutMe.SUMMARY}
-            basicInfo={aboutMe.basicInfo}
-            skills={aboutMe.skills}
-          />
-          <Experiences
-            title={experiences.TITLE}
-            timeline={experiences.timeline}
-          />
-          <Projects
-            title={projects.TITLE}
-            items={projects.items}
-          />
-          <Educations
-            title={educations.TITLE}
-            timeline={educations.timeline}
-          />
-          <Footer
-            slogan={footer.SLOGAN}
-            copyright={footer.COPYRIGHT}
-            socialNetworks={app.socialNetworks}
-            logoUrl={app.logoUrl}
-          />
-        </>
-      )}
-    </>
-  );
-};
-
-export default memo<Props>(CSSModules(App, styles));
+export default memo<Props>(App);
